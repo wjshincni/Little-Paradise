2e4109cfc472fa530d0f4c9b36e50088d26ffd6c
ttx <105620083+wjshincni@users.noreply.github.com>
2024-01-07 19:19:39 +0800

    Add files via upload

---
+++ index/index.js
@@ -0,0 +1,144 @@
+//js
+function loadPage1() {
+    // 获取当前页面元素
+    var currentPage = document.getElementById("startPage");
+    var targetPage = document.getElementById("page1");
+    page1.scrollIntoView({ behavior: 'smooth'},{});
+    // 移除当前页面的显示类名
+    currentPage.classList.remove("show");
+    // 等待一定时间后加载目标页面
+    setTimeout(function(){
+      // 添加显示类名
+      targetPage.classList.add("show");
+    }, 1000); // 此处的ms可根据需求调整
+}
+function loadPage2() {
+    // 获取当前页面元素
+    var currentPage = document.getElementById("page1");
+    var targetPage = document.getElementById("page2");
+    page2.scrollIntoView({ behavior: 'smooth' });
+    // 移除当前页面的显示类名
+    currentPage.classList.remove("show");
+    // 等待一定时间后加载目标页面
+    setTimeout(function(){
+      // 添加显示类名
+      targetPage.classList.add("show");
+    }, 2000); // 此处的ms可根据需求调整
+}
+function loadPage3() {
+    // 获取当前页面元素
+    var currentPage = document.getElementById("page2");
+    var targetPage = document.getElementById("page3");
+    page3.scrollIntoView({ behavior: 'smooth' });
+    // 移除当前页面的显示类名
+    currentPage.classList.remove("show");
+    setTimeout(function(){
+      // 获取目标页面元素
+      // 添加显示类名
+      targetPage.classList.add("show");
+    }, 2000); // 此处的500ms可根据需求调整
+}
+function loadPage4() {
+    // 获取当前页面元素
+    var currentPage = document.getElementById("page3");
+    var targetPage = document.getElementById("page4");
+    page4.scrollIntoView({ behavior: 'smooth' });
+    // 移除当前页面的显示类名
+    currentPage.classList.remove("show");
+    setTimeout(function(){
+      // 获取目标页面元素
+      // 添加显示类名
+      targetPage.classList.add("show");
+    }, 2000); // 此处的500ms可根据需求调整
+}
+function loadPage5() {
+  // 获取当前页面元素
+  var currentPage = document.getElementById("page4");
+  var targetPage = document.getElementById("page5");
+  page5.scrollIntoView({ behavior: 'smooth'},{});
+  // 移除当前页面的显示类名
+  currentPage.classList.remove("show");
+  // 等待一定时间后加载目标页面
+  setTimeout(function(){
+    // 添加显示类名
+    targetPage.classList.add("show");
+  }, 1000); // 此处的ms可根据需求调整
+}
+function loadPage6() {
+  // 获取当前页面元素
+  var currentPage = document.getElementById("page5");
+  var targetPage = document.getElementById("page6");
+  page6.scrollIntoView({ behavior: 'smooth'},{});
+  // 移除当前页面的显示类名
+  currentPage.classList.remove("show");
+  // 等待一定时间后加载目标页面
+  setTimeout(function(){
+    // 添加显示类名
+    targetPage.classList.add("show");
+  }, 1000); // 此处的ms可根据需求调整
+}
+function loadPage7() {
+  // 获取当前页面元素
+  var currentPage = document.getElementById("page6");
+  var targetPage = document.getElementById("page7");
+  page7.scrollIntoView({ behavior: 'smooth'},{});
+  // 移除当前页面的显示类名
+  currentPage.classList.remove("show");
+  // 等待一定时间后加载目标页面
+  setTimeout(function(){
+    // 添加显示类名
+    targetPage.classList.add("show");
+  }, 1000); // 此处的ms可根据需求调整
+}
+function loadPage8() {
+  // 获取当前页面元素
+  var currentPage = document.getElementById("page7");
+  var targetPage = document.getElementById("page8");
+  page8.scrollIntoView({ behavior: 'smooth'},{});
+  // 移除当前页面的显示类名
+  currentPage.classList.remove("show");
+  // 等待一定时间后加载目标页面
+  setTimeout(function(){
+    // 添加显示类名
+    targetPage.classList.add("show");
+  }, 1000); // 此处的ms可根据需求调整
+}
+function loadPage9() {
+  // 获取当前页面元素
+  var currentPage = document.getElementById("page8");
+  var targetPage = document.getElementById("page9");
+  page9.scrollIntoView({ behavior: 'smooth'},{});
+  // 移除当前页面的显示类名
+  currentPage.classList.remove("show");
+  // 等待一定时间后加载目标页面
+  setTimeout(function(){
+    // 添加显示类名
+    targetPage.classList.add("show");
+  }, 1000); // 此处的ms可根据需求调整
+}
+function loadPage10() {
+  // 获取当前页面元素
+  var currentPage = document.getElementById("page9");
+  var targetPage = document.getElementById("page10");
+  page10.scrollIntoView({ behavior: 'smooth'},{});
+  // 移除当前页面的显示类名
+  currentPage.classList.remove("show");
+  // 等待一定时间后加载目标页面
+  setTimeout(function(){
+    // 添加显示类名
+    targetPage.classList.add("show");
+  }, 1000); // 此处的ms可根据需求调整
+}
+function loadPage11() {
+  // 获取当前页面元素
+  var currentPage = document.getElementById("page10");
+  var targetPage = document.getElementById("page11");
+  page11.scrollIntoView({ behavior: 'smooth'},{});
+  // 移除当前页面的显示类名
+  currentPage.classList.remove("show");
+  // 等待一定时间后加载目标页面
+  setTimeout(function(){
+    // 添加显示类名
+    targetPage.classList.add("show");
+  }, 1000); // 此处的ms可根据需求调整
+}
